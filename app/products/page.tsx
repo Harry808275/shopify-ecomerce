c7f2277e248528c88a03d6cbc8a2065724a17113
Harry808275 <harry808275@gmail.com>
2025-12-01 09:12:17 -0800

    Fix TypeScript product type in products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -32,7 +32,7 @@ export default async function ProductsPage({
 
         {/* Products Grid */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-          {products.map((product) => (
+          {products.map((product: any) => (
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
